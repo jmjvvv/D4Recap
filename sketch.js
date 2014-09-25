83c134f8c96a01547566b1d8261240b6fe2cd141
jmjvvv <jmjvvv@gmail.com>
2014-09-25 13:28:40 +0900

    df

--- sketch.js
+++ sketch.js
@@ -2,4 +2,12 @@ function setup()
 {
 	createCanvas(800, 600);
 	background(0);
+	noFill();
+	stroke(255, 0, 0);
+	ellipse(400, 300, 100, 100);
+}
+
+function draw()
+{
+	ellipse(400, 300, mouseX, mouseY);
 }
